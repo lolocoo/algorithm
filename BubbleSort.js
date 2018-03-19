ff4ae46edbbb7d8905dd5d42e06eea8ed0c4c4ed
lolocoo <lolocoo@gmail.com>
2018-03-19 15:22:45 +0800

    Create BubbleSort.js

---
+++ BubbleSort.js
@@ -0,0 +1,14 @@
+var arr = [7,6,5,4,3,2,1];
+
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      }
+    }
+  }
+}
+
+bubbleSort(arr);
