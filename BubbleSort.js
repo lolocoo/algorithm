ec93af47a8c212cf6c8c6a9e7ec94c5a2f1394fe
lolocoo <lolocoo@gmail.com>
2018-03-19 15:30:50 +0800

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -3,7 +3,7 @@ var arr = [7,6,5,4,3,2,1];
 function bubbleSort(arr) {
   let len = arr.length;
   for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len - 1; j++) {
+    for (let j = 0; j < len - i - 1; j++) { //从内♻️减去外♻️跑过的轮数
       if (arr[j] > arr[j+1]) {
         [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
       }
